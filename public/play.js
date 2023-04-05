41116346003c4a831dd9720cc8f8d1735e02d6a6
Spencer Peart <spencer.peart@gmail.com>
2023-04-05 17:37:55 -0600

    add api call and data parsing

--- public/play.js
+++ public/play.js
@@ -1,220 +1,215 @@
-// (function(){
-//     // Functions
-//     function buildQuiz(){
-//       // variable to store the HTML output
-//       const output = [];
+(function(){
+    // Functions
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
   
-//       // for each question...
-//       myQuestions.forEach(
-//         (currentQuestion, questionNumber) => {
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
   
-//           // variable to store the list of possible answers
-//           const answers = [];
+          // variable to store the list of possible answers
+          const answers = [];
   
-//           // and for each available answer...
-//           for(letter in currentQuestion.answers){
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
   
-//             // ...add an HTML radio button
-//             answers.push(
-//               `<label>
-//                 <input type="radio" name="question${questionNumber}" value="${letter}">
-//                 ${letter} :
-//                 ${currentQuestion.answers[letter]}
-//               </label>`
-//             );
-//           }
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
   
-//           // add this question and its answers to the output
-//           output.push(
-//             `<div class="slide">
-//               <div class="question"> ${currentQuestion.question} </div>
-//               <div class="answers"> ${answers.join("")} </div>
-//             </div>`
-//           );
-//         }
-//       );
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+      );
   
-//       // finally combine our output list into one string of HTML and put it on the page
-//       quizContainer.innerHTML = output.join('');
-//     }
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
   
-//     function showResults(){
+    function showResults(){
   
-//       // gather answer containers from our quiz
-//       const answerContainers = quizContainer.querySelectorAll('.answers');
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
   
-//       // keep track of user's answers
-//       let numCorrect = 0;
+      // keep track of user's answers
+      let numCorrect = 0;
   
-//       // for each question...
-//       myQuestions.forEach( (currentQuestion, questionNumber) => {
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
   
-//         // find selected answer
-//         const answerContainer = answerContainers[questionNumber];
-//         const selector = `input[name=question${questionNumber}]:checked`;
-//         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
   
-//         // if answer is correct
-//         if(userAnswer === currentQuestion.correctAnswer){
-//           // add to the number of correct answers
-//           numCorrect++;
-//           // updateScore(*numCorrect);
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          // updateScore(*numCorrect);
   
-//           // color the answers green
-//           answerContainers[questionNumber].style.color = 'lightgreen';
-//         }
-//         // if answer is wrong or blank
-//         else{
-//           // color the answers red
-//           answerContainers[questionNumber].style.color = 'red';
-//           saveScore(numCorrect);
-//         }
-//       });
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+          saveScore(numCorrect);
+        }
+      });
   
-//       // show number of correct answers out of total
-//       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-//     }
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
 
-//     function saveScore(score) {
-//       const userName = this.getPlayerName();
-//       let scores = [];
-//       const scoresText = localStorage.getItem('scores');
-//       if (scoresText) {
-//         scores = JSON.parse(scoresText);
-//       }
-//       scores = updateScores(userName, score, scores);
+    function saveScore(score) {
+      const userName = this.getPlayerName();
+      let scores = [];
+      const scoresText = localStorage.getItem('scores');
+      if (scoresText) {
+        scores = JSON.parse(scoresText);
+      }
+      scores = updateScores(userName, score, scores);
   
-//       localStorage.setItem('scores', JSON.stringify(scores));
-//     }
+      localStorage.setItem('scores', JSON.stringify(scores));
+    }
 
-//     function updateScores(userName, score, scores) {
-//       const date = new Date().toLocaleDateString();
-//       const newScore = { name: userName, score: score, date: date };
+    function updateScores(userName, score, scores) {
+      const date = new Date().toLocaleDateString();
+      const newScore = { name: userName, score: score, date: date };
   
-//       let found = false;
-//       for (const [i, prevScore] of scores.entries()) {
-//         if (score > prevScore.score) {
-//           scores.splice(i, 0, newScore);
-//           found = true;
-//           break;
-//         }
-//       }
+      let found = false;
+      for (const [i, prevScore] of scores.entries()) {
+        if (score > prevScore.score) {
+          scores.splice(i, 0, newScore);
+          found = true;
+          break;
+        }
+      }
   
-//       if (!found) {
-//         scores.push(newScore);
-//       }
+      if (!found) {
+        scores.push(newScore);
+      }
   
-//       if (scores.length > 10) {
-//         scores.length = 10;
-//       }
+      if (scores.length > 10) {
+        scores.length = 10;
+      }
   
-//       return scores;
-//     }
+      return scores;
+    }
 
-//     // updateScore(*numCorrect) {
-//     //   const scoreEl = document.querySelector('#score');
-//     //   scoreEl.textContent = score;
-//     // }
+    // updateScore(*numCorrect) {
+    //   const scoreEl = document.querySelector('#score');
+    //   scoreEl.textContent = score;
+    // }
   
-//     function showSlide(n) {
-//       slides[currentSlide].classList.remove('active-slide');
-//       slides[n].classList.add('active-slide');
-//       currentSlide = n;
-//       if(currentSlide === 0){
-//         previousButton.style.display = 'none';
-//       }
-//       else{
-//         previousButton.style.display = 'inline-block';
-//       }
-//       if(currentSlide === slides.length-1){
-//         nextButton.style.display = 'none';
-//         submitButton.style.display = 'inline-block';
-//       }
-//       else{
-//         nextButton.style.display = 'inline-block';
-//         submitButton.style.display = 'none';
-//       }
-//     }
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
   
-//     function showNextSlide() {
-//       showSlide(currentSlide + 1);
-//     }
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
   
-//     function showPreviousSlide() {
-//       showSlide(currentSlide - 1);
-//     }
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+
+    function parseData(data) {
+      const obj = document.getElementById('quiz');
+      obj.textContent = data.results[0].category;
+      myQuestions.pop();
+      myQuestions.push(data.results[0]);
+      return;
+    }
+
+    function aFunction(data){
+
+      callService("https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple", parseData);
+    
+    }
+    
+    function callService(url, displayCallback) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          displayCallback(data);
+        });
+    }
+
+    function addToQuestions(data) {
+
+    }
   
-//     // Variables
-//     const quizContainer = document.getElementById('quiz');
-//     const resultsContainer = document.getElementById('results');
-//     const submitButton = document.getElementById('submit');
-//     const myQuestions = [
-//       {
-//         question: "Which artist is from Canada?",
-//         answers: {
-//           a: "Drake",
-//           b: "Morgan Wallen",
-//           c: "Hollow Coves",
-//           d: "Taylor Swift"
-//         },
-//         correctAnswer: "c"
-//       },
-//     ];
+    // Variables
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Which artist is from Canada?",
+        answers: {
+          a: "Drake",
+          b: "Morgan Wallen",
+          c: "Hollow Coves",
+          d: "Taylor Swift"
+        },
+        correctAnswer: "c"
+      },
+    ];
   
-//     buildQuiz();
+    buildQuiz();
   
-//     // Pagination
-//     const previousButton = document.getElementById("previous");
-//     const nextButton = document.getElementById("next");
-//     const slides = document.querySelectorAll(".slide");
-//     let currentSlide = 0;
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
   
-//     // Show the first slide
-//     showSlide(currentSlide);
+    // Show the first slide
+    showSlide(currentSlide);
   
-//     // Event listeners
-//     submitButton.addEventListener('click', showResults);
-//     previousButton.addEventListener("click", showPreviousSlide);
-//     nextButton.addEventListener("click", showNextSlide);
-//   })();
+    // Event listeners
+    submitButton.addEventListener('click', aFunction);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
 
 
 
 
-function aFunction(data){
-
-  const questionEl = document.createElement();
-  const answerEl = document.createElement();
-
-
-
-
-}
-
-
-
-
-
-
-function callService(url, displayCallback) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      displayCallback(data);
-    });
-}
-
-const random = Math.floor(Math.random() * 1000);
-callService(
-  `https://picsum.photos/v2/list?page=${random}&limit=1`,
-  displayPicture
-);
-callService("https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple", aFunction);
 
 
 
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-
-  
